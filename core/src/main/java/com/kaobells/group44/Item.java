95c3d375ef83b45c16983bab2c70d528becd00a1
Sasha Kosic <skosic2@uwo.ca>
2024-11-25 20:14:52 -0500

    Added function to format JSON in main.java to be modified. Added database JSON to assets, Coded Item class.

--- core/src/main/java/com/kaobells/group44/Item.java
+++ core/src/main/java/com/kaobells/group44/Item.java
@@ -1,20 +1,68 @@
 package com.kaobells.group44;
 
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-
 public class Item {
+    public int itemID;  // Item identifier
+    public String itemStat; // Stat that Item will change
+    public float itemStatValue; //Float for calculating how much item will change stat
 
-    ImageButton image;
+    public Item(String ItemID){ //Constructor
+    this.itemID = itemID;
+    setItemValues(itemID);
+    }
 
-    //
-    public int id;
+    public void setItemValues(int ItemID){ //based on item ID fills in stats
+        switch (ItemID){
+            //case 0 cup noodle food item
+            case 0:
+                this.itemStat = "fullness";
+                this.itemStatValue = 1.0f;
+                break;
 
-    // a number corresponding to which stat bar the item will boost
-    public int type;
+            //case 1 spoke bagel food item
+            case 1:
+                this.itemStat = "fullness";
+                this.itemStatValue = 2.0f;
+                break;
 
+            //case 2 shawarma food item
+            case 2:
+                this.itemStat = "fullness";
+                this.itemStatValue = 3.0f;
+                break;
 
-    public String name;
-    public String description;
-    public int price;
+            //case 3 normal duck gift item
+            case 3:
+                this.itemStat = "happiness";
+                this.itemStatValue = 1.0f;
+                break;
 
+            //case 4 cool duck gift item
+            case 4:
+                this.itemStat = "happiness";
+                this.itemStatValue = 2.0f;
+                break;
+
+            //case 5 super cool duck gift item
+            case 5:
+                this.itemStat = "happiness";
+                this.itemStatValue = 3.0f;
+                break;
+        }
+    }
+
+    public void setItemStat(String itemStat){
+        this.itemStat = itemStat;
+    }
+
+    public String getItemStat(){
+        return this.itemStat;
+    }
+
+    public float getItemStatValue(){
+        return this.itemStatValue;
+    }
+
+    public int getItemID(){
+        return this.itemID;
+    }
 }
