ef90793afd2d9675b29e028e41b0ee717012cc5a
Joshua Beyer <jbeyer@uwo.ca>
2024-11-29 20:17:14 -0500

    Fixed main menu asset handling, formating, added parental controls + instructions button + character heads. Corrected credit screen images and credit image sizing.

---
+++ core/src/main/java/com/kaobells/group44/Assets.java
@@ -0,0 +1,32 @@
+package com.kaobells.group44;
+
+public class Assets {
+
+    // Fonts
+    public static final String FONT_TITLE = "fonts/title-font.fnt";
+    public static final String FONT_BODY = "fonts/body-font.fnt";
+
+    // Music
+    public static final String MUSIC_MAIN_MENU = "music/jb-sample-lowqual.mp3";
+    public static final String MUSIC_STORY = "music/story-theme.mp3";
+
+    // Sound Effects
+    public static final String SOUND_BUTTON_CLICK = "sounds/button-click.wav";
+    public static final String SOUND_GAME_OVER = "sounds/game-over.wav";
+
+    // Textures
+    public static final String TEXTURE_BACKGROUND_MAIN = "textures/main-background.png";
+    public static final String TEXTURE_BACKGROUND_STORY = "textures/story-background.png";
+
+    // Buttons
+    public static final String BUTTON_START_GAME = "startScreen/start-game-btn.png";
+    public static final String BUTTON_EXIT = "startScreen/exit-btn.png";
+    public static final String BUTTON_PARENTAL_CONTROLS = "startScreen/parental-controls-btn.png";
+
+    // Character Heads
+    public static final String HEAD_BRAVE = "characters/brave-head.png";
+    public static final String HEAD_HASTY = "characters/hasty-head.png";
+    public static final String HEAD_QUIRKY = "characters/quirky-head.png";
+    public static final String HEAD_RELAXED = "characters/relaxed-head.png";
+    public static final String HEAD_SERIOUS = "characters/serious-head.png";
+}
