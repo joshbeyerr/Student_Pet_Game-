7b20e53acdb82a517575aae09bbbabe7132536c1
Sasha Kosic <skosic2@uwo.ca>
2024-12-04 18:04:51 -0500

    g

---
+++ core/src/test/java/com/kaobells/group44/ItemTest.java
@@ -0,0 +1,50 @@
+package com.kaobells.group44;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @org.junit.jupiter.api.Test
+    void setItemValues() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItemStatValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItemID() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reduceCount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void increaseCount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setItemCount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getItemCount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isFood() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isGift() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getImage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setImage() {
+    }
+}
